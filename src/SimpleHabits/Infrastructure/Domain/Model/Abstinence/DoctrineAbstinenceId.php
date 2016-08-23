9e6197cecfbfdcbac9e01de6623674916ab92c04
Maks Rafalko <b0rn@list.ru>
2016-08-23 23:57:27 +0300

    Create abstinence ui form (#11)
    * Create abstinence UI form
    *  	Add doctrine integration, repository, custom type for ValueObject Id
    * Add code to save abstinence on form submit
    
    * Applied fixes from StyleCI
    
    * Add doctrine integration, repository, custom type for ValueObject Id. Add code to save abstinence on form submit
    
    * Applied fixes from StyleCI

---
+++ src/SimpleHabits/Infrastructure/Domain/Model/Abstinence/DoctrineAbstinenceId.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace SimpleHabits\Infrastructure\Domain\Model\Abstinence;
+
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\GuidType;
+use SimpleHabits\Domain\Model\Abstinence\AbstinenceId;
+
+/**
+ * Class DoctrineAbstinenceId.
+ */
+class DoctrineAbstinenceId extends GuidType
+{
+    /**
+     * @param $value
+     * @param AbstractPlatform $platform
+     *
+     * @return mixed
+     */
+    public function convertToDatabaseValue($value, AbstractPlatform $platform)
+    {
+        return $value->id();
+    }
+
+    /**
+     * @param $value
+     * @param AbstractPlatform $platform
+     *
+     * @return AbstinenceId
+     */
+    public function convertToPHPValue($value, AbstractPlatform $platform)
+    {
+        return new AbstinenceId($value);
+    }
+}
---
+++ src/SimpleHabits/Infrastructure/Domain/Model/Abstinence/DoctrineAbstinenceRepository.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace SimpleHabits\Infrastructure\Domain\Model\Abstinence;
+
+use Doctrine\ORM\EntityManagerInterface;
+use SimpleHabits\Domain\Model\Abstinence\Abstinence;
+use SimpleHabits\Domain\Model\Abstinence\AbstinenceId;
+use SimpleHabits\Domain\Model\Abstinence\AbstinenceRepository;
+
+class DoctrineAbstinenceRepository implements AbstinenceRepository
+{
+    /**
+     * @var EntityManagerInterface
+     */
+    private $em;
+
+    /**
+     * DoctrineAbstinenceRepository constructor.
+     *
+     * @param EntityManagerInterface $em
+     */
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function add(Abstinence $abstinence)
+    {
+        // TODO check whether we should write a spec for it or no
+
+        $this->em->persist($abstinence);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function remove(Abstinence $abstinence)
+    {
+        // TODO complete
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function findById(AbstinenceId $id)
+    {
+        // TODO complete
+    }
+}
