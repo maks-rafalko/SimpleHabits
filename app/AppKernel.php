70bba6c964281e406caf18ccda2ea1a2a42233c4
born_free <b0rn@list.ru>
2016-10-01 09:51:56 +0300

    Add migrations bundle and add FK user_id in `abstinences` table

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
 
             new League\Tactician\Bundle\TacticianBundle(),
+            new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
---
+++ src/SimpleHabits/Infrastructure/Persistence/Doctrine/DoctrineMigrations/Version20161001063930.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace SimpleHabits\Infrastructure\Persistence\Doctrine;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20161001063930 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE abstinences ADD CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users(id)');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE `abstinences` DROP FOREIGN KEY `fk_user_id`;');
+        $this->addSql('ALTER TABLE `abstinences` DROP INDEX `fk_user_id`;');
+    }
+}
