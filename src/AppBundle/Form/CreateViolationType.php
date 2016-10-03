e5f46de0ff343d9e46f3a62fa65fa091302fb773
born_free <b0rn@list.ru>
2016-10-03 22:54:28 +0300

    Violation routes, view abstinence page

--- src/AppBundle/Form/CreateViolationType.php
+++ src/AppBundle/Form/CreateViolationType.php
@@ -2,19 +2,17 @@
 
 namespace AppBundle\Form;
 
-use SimpleHabits\Application\Command\CreateNewAbstinenceCommand;
-use SimpleHabits\Domain\Model\Abstinence\Abstinence;
-use SimpleHabits\Domain\Model\User\UserId;
+use SimpleHabits\Application\Command\AddViolationCommand;
+use SimpleHabits\Domain\Model\Violation\Violation;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\Length;
-use Symfony\Component\Validator\Constraints\NotBlank;
 
-class CreateAbstinenceType extends AbstractType
+class CreateViolationType extends AbstractType
 {
     /**
      * @param FormBuilderInterface $builder
@@ -24,16 +22,13 @@ class CreateAbstinenceType extends AbstractType
     {
         $builder
             ->add(
-                'name',
+                'reason',
                 TextType::class,
                 [
-                    'empty_data' => '',
                     'required' => false,
                     'constraints' => [
-                        new NotBlank(),
                         new Length([
-                            'min' => Abstinence::NAME_MIN_LENGTH,
-                            'max' => Abstinence::NAME_MAX_LENGTH,
+                            'max' => Violation::MAX_REASON_LENGTH,
                         ]),
                     ],
                 ]
@@ -47,18 +42,26 @@ class CreateAbstinenceType extends AbstractType
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'user' => null,
-            'data_class' => CreateNewAbstinenceCommand::class,
+            'abstinence' => null,
+            'data_class' => AddViolationCommand::class,
             'empty_data' => function (FormInterface $form) {
-                $userId = $form->getConfig()->getOption('user')->getId();
+                $abstinenceId = $form->getConfig()->getOption('abstinence')->getId();
 
-                return new CreateNewAbstinenceCommand(
-                    new UserId($userId),
-                    $form->get('name')->getData()
+                return new AddViolationCommand(
+                    $abstinenceId,
+                    $form->get('reason')->getData()
                 );
             },
         ]);
 
-        $resolver->setRequired(['user']);
+        $resolver->setRequired(['abstinence']);
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'app_create_violation_type';
     }
 }
