de808f47dfb26264b4891f184cd8b12bd8c8e23e
Maks Rafalko <b0rn@list.ru>
2016-10-03 23:00:20 +0300

    Abstinence text (#19)
    * Violation routes, view abstinence page
    
    * Applied fixes from StyleCI
    
    * Change route paths

--- src/AppBundle/Controller/AbstinencesController.php
+++ src/AppBundle/Controller/AbstinencesController.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 namespace AppBundle\Controller;
 
+use AppBundle\Form\CreateViolationType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
-use SimpleHabits\Application\Command\AddViolationCommand;
 use SimpleHabits\Domain\Model\Abstinence\Abstinence;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -29,6 +29,17 @@ class AbstinencesController extends Controller
     }
 
     /**
+     * @Route("/abstinences/{id}", name="abstinence")
+     * @Security("user.getId().equals(abstinence.getUserId())")
+     */
+    public function viewAction(Abstinence $abstinence)
+    {
+        return $this->render('abstinence/view.html.twig', [
+            'abstinence' => $abstinence,
+        ]);
+    }
+
+    /**
      * @Route("/abstinences/create", name="abstinences_create")
      */
     public function createAction(Request $request)
@@ -47,14 +58,41 @@ class AbstinencesController extends Controller
     }
 
     /**
-     * @Route("/abstinences/{id}/violate", name="abstinences_violate")
+     * @Route("/abstinences/{id}/violations/new", name="abstinences_violates_new", methods={"GET"})
      * @Security("user.getId().equals(abstinence.getUserId())")
      */
-    public function violateAction(Abstinence $abstinence)
+    public function addViolationAction(Abstinence $abstinence, Request $request)
     {
-        $command = new AddViolationCommand($abstinence->getId());
-        $this->get('tactician.commandbus')->handle($command);
+        $form = $this->createForm(CreateViolationType::class, null, [
+            'action' => $this->generateUrl('abstinences_violate', ['id' => $abstinence->getId()]),
+        ]);
+        $form->handleRequest($request);
 
-        return $this->redirectToRoute('abstinences');
+        return $this->render('abstinence/add-violation.html.twig', [
+            'abstinence' => $abstinence,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/abstinences/{id}/violations", name="abstinences_violate", methods={"POST"})
+     * @Security("user.getId().equals(abstinence.getUserId())")
+     */
+    public function violateAction(Abstinence $abstinence, Request $request)
+    {
+        $form = $this->createForm(CreateViolationType::class, null, ['abstinence' => $abstinence]);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $command = $form->getData();
+            $this->get('tactician.commandbus')->handle($command);
+
+            return $this->redirectToRoute('abstinences');
+        }
+
+        return $this->render('abstinence/add-violation.html.twig', [
+            'abstinence' => $abstinence,
+            'form' => $form->createView(),
+        ]);
     }
 }
