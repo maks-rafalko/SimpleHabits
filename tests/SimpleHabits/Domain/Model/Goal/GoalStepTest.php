09f9068a75c3abdd304a9a04d0c0ce12949eac4c
born_free <b0rn@list.ru>
2016-11-13 01:48:40 +0300

    Add Codecov integration, PHPUnit

---
+++ tests/SimpleHabits/Domain/Model/Goal/GoalStepTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace SimpleHabits\Domain\Model\Goal;
+
+
+class GoalStepTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var GoalStep
+     */
+    private $goalStep;
+
+    protected function setUp()
+    {
+        $this->goalStep = new GoalStep(new GoalStepId(), 100, new \DateTimeImmutable());
+    }
+
+    /**
+     * @test
+     */
+    public function it_correctly_returns_recorded_at_date()
+    {
+        $recordedAt = $this->goalStep->getRecordedAt();
+
+        $this->assertInstanceOf(\DateTimeInterface::class, $recordedAt);
+    }
+}
