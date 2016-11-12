b8732d733f0b12f657a4fdba558db07cdd168273
born_free <b0rn@list.ru>
2016-11-13 02:28:43 +0300

    Codecov integration, new tests

--- tests/SimpleHabits/Domain/Model/Goal/GoalStepTest.php
+++ tests/SimpleHabits/Domain/Model/Goal/GoalStepTest.php
@@ -18,6 +18,26 @@ class GoalStepTest extends \PHPUnit_Framework_TestCase
     /**
      * @test
      */
+    public function it_correctly_returns_id()
+    {
+        $id = $this->goalStep->getId();
+
+        $this->assertInstanceOf(GoalStepId::class, $id);
+    }
+
+    /**
+     * @test
+     */
+    public function it_correctly_returns_value()
+    {
+        $value = $this->goalStep->getValue();
+
+        $this->assertEquals(100, $value);
+    }
+
+    /**
+     * @test
+     */
     public function it_correctly_returns_recorded_at_date()
     {
         $recordedAt = $this->goalStep->getRecordedAt();
